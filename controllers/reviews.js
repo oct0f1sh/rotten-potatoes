137490fc1df51e8fb932f0b67f85c8d2db116980
Duncan <buzzbuzz.macdonald@gmail.com>
2018-11-05 15:03:06 -0800

    done

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -66,4 +66,12 @@ module.exports = function(app) {
             console.log(err.message);
         })
     })
+
+    app.delete('/reviews/comments/:id', (req, res) => {
+        Comment.findByIdAndRemove(req.params.id).then((comment) => {
+            res.redirect(`/reviews/${comment.reviewId}`)
+        }).catch((err) => {
+            console.log(err.message)
+        })
+    })
 }
