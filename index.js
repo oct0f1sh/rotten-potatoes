51878cefa573926e1cf99fc51b151335029fa27f
Duncan <buzzbuzz.macdonald@gmail.com>
2018-11-05 12:13:49 -0800

    progress

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 const express = require('express')
 const app = express()
 var exphbs = require('express-handlebars')
+const mongoose = require('mongoose')
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-let reviews = [ {title: "Bad review", movieTitle: "Lame" },
-                {title: "another review", movieTitle: "Swag" }]
+mongoose.connect('mongodb://localhost/rotten-potatoes');
+
+const Review = mongoose.model('Review', {
+    title: String,
+    movieTitle: String
+});
+
+// let reviews = [ {title: "Bad review", movieTitle: "Lame" },
+//                 {title: "another review", movieTitle: "Swag" }]
 
 app.get('/', (req, res) => {
-    res.render('reviews-index', {reviews: reviews })
-})
+    Review.find().then(reviews => {
+        res.render('reviews-index', {reviews: reviews });
+    }).catch(err => {
+        console.log(err);
+    });
+});
 
 app.listen(3000, () => {
     console.log('look at me listening on port 3000')
