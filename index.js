6c19d34726ad3649c191f8e8e923558b85f08c70
Duncan <buzzbuzz.macdonald@gmail.com>
2018-11-05 12:37:34 -0800

    ugh

--- index.js
+++ index.js
@@ -2,19 +2,20 @@ const express = require('express')
 const app = express()
 var exphbs = require('express-handlebars')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 mongoose.connect('mongodb://localhost/rotten-potatoes');
 
+app.use(bodyParser.urlencoded({extended: true}));
+
 const Review = mongoose.model('Review', {
     title: String,
-    movieTitle: String
+    movieTitle: String,
+    description: String
 });
-
-// let reviews = [ {title: "Bad review", movieTitle: "Lame" },
-//                 {title: "another review", movieTitle: "Swag" }]
 
 app.get('/', (req, res) => {
     Review.find().then(reviews => {
@@ -24,6 +25,27 @@ app.get('/', (req, res) => {
     });
 });
 
+app.get('/reviews/new', (req, res) => {
+    res.render('reviews-new', {});
+});
+
+app.post('/reviews', (req, res) => {
+    Review.create(req.body).then((review) => {
+        console.log(review);
+        res.redirect(`/reviews/${review._id}`);
+    }).catch((err) => {
+        console.log(err.message);
+    })
+});
+
+app.get('/reviews/:id', (req, res) => {
+    Review.findById(req.params.id).then((review) => {
+        res.render('reviews-show', {review: review});
+    }).catch((err) => {
+        console.log(err.message);
+    });
+})
+
 app.listen(3000, () => {
     console.log('look at me listening on port 3000')
-})
+});
